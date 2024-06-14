006ab26d4b40678634b92441fc225fc75ce0bad3
smbhathiya <smbhathiya0@gmail.com>
2024-06-14 18:42:02 +0530

    Add project files.

---
+++ Debra/WebForm.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Debra
+{
+    public partial class WebForm : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            // Create an instance of the web service client
+            HelloService.WebServiceHelloSoapClient client = new HelloService.WebServiceHelloSoapClient();
+
+            // Call the HelloWorld method
+            string result = client.HelloWorld();
+
+            // Display the result in Label1
+            Label1.Text = result;
+        }
+    }
+}
---
+++ DesktopApplication/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DesktopApplication
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Create an instance of the web service client
+            HelloService.WebServiceHelloSoapClient client = new HelloService.WebServiceHelloSoapClient();
+
+            // Call the HelloWorld method
+            string result = client.HelloWorld();
+
+            // Display the result in Label1
+            label1.Text = result;
+        }
+    }
+}
